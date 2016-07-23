1e7044d5897f4ece576fa6bb5f090f6ca5b93e1b
Daniel ILLESCAS ROMERO <Daniel@daniel.home>
2016-07-23 20:51:50 +0200

    Added music switch ui test Assertion

--- QuestionsUITests/QuestionsUITests.swift
+++ QuestionsUITests/QuestionsUITests.swift
@@ -16,12 +16,25 @@ class QuestionsUITests: XCTestCase {
 		
 		let tablesQuery = app.tables
 		let backgroundMusicSwitch = tablesQuery.switches["Background music"]
+		
+		var switchState1 = backgroundMusicSwitch.value as! String
+
 		backgroundMusicSwitch.tap()
+		
+		let switchState2 = backgroundMusicSwitch.value as! String
+		
+		XCTAssert((switchState1 == "1" && switchState2 == "0") || (switchState1 == "0" && switchState2 == "1"),
+		          "Switch button not working")
+		
 		backgroundMusicSwitch.tap()
+		
+		switchState1 = backgroundMusicSwitch.value as! String
+		
+		XCTAssert((switchState1 == "1" && switchState2 == "0") || (switchState1 == "0" && switchState2 == "1"),
+		          "Switch button not working")
 		
 		app.navigationBars["Settings"].buttons["Main menu"].tap()
 		app.buttons["INSTRUCTIONS"].tap()
-
 		app.alerts["Instructions"].collectionViews.buttons["OK"].tap()
 		app.buttons["START GAME"].tap()
 		app.tables.staticTexts["Social"].tap()
