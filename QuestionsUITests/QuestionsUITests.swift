59dbbfe5490a3766dcf6bd821f2b0ac6b8a18dd3
Daniel ILLESCAS ROMERO <Daniel@daniel.home>
2016-07-23 18:36:28 +0200

    Added UI Test class to test main buttons

---
+++ QuestionsUITests/QuestionsUITests.swift
@@ -0,0 +1,36 @@
+import XCTest
+
+class QuestionsUITests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        XCUIApplication().launch()
+    }
+    
+    func testButtons() {
+		
+		XCUIDevice.sharedDevice().orientation = .Portrait
+		
+		let app = XCUIApplication()
+		app.buttons["SETTINGS"].tap()
+		
+		let tablesQuery = app.tables
+		let backgroundMusicSwitch = tablesQuery.switches["Background music"]
+		backgroundMusicSwitch.tap()
+		backgroundMusicSwitch.tap()
+		app.navigationBars["Settings"].buttons["Main menu"].tap()
+		app.buttons["INSTRUCTIONS"].tap()
+
+		app.alerts["Instructions"].collectionViews.buttons["OK"].tap()
+		app.buttons["START GAME"].tap()
+		app.tables.staticTexts["Social"].tap()
+		
+		let pauseButton = app.buttons["Pause"]
+		pauseButton.tap()
+		app.buttons["Go back"].tap()
+		app.tables.staticTexts["Technology"].tap()
+		pauseButton.tap()
+		app.buttons["Main menu"].tap()
+    }
+    
+}
