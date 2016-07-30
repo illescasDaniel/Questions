4c50181314293ae6e0baeaf012dcd045b2934e92
Daniel ILLESCAS ROMERO <Daniel@daniel.home>
2016-07-30 15:21:07 +0200

    Removed unnecessary variables in Quiz struct

--- Questions/Quiz.swift
+++ Questions/Quiz.swift
@@ -1,10 +1,5 @@
 import Foundation
 
 struct Quiz {
-
 	static let set = NSArray(contentsOfFile: NSBundle.mainBundle().pathForResource("Quiz", ofType: "plist")!)!
-
-	var question: String
-	var answers: [String]
-	var answer: Int
 }
