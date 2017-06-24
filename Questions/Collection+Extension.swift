3218306d1147843db20f79d53ed46e6b07db89ee
illescasDaniel <illescas.daniel@protonmail.com>
2017-06-24 23:09:11 +0200

    Improved shuffle/d methods

--- Questions/Collection+Extension.swift
+++ Questions/Collection+Extension.swift
@@ -2,33 +2,14 @@ import Foundation
 import GameplayKit // .shuffled
 
 extension Collection {
-	
-	/// Return a copy of `self` with its elements shuffled
 	func shuffled() -> [Iterator.Element] {
-		
-		if #available(iOS 10.0, *) {
-			let shuffledArray = (self as? NSArray)?.shuffled()
-			return shuffledArray as! [Self.Iterator.Element]
-		}
-		
-		var list = Array(self)
-		list.shuffleInPlace()
-		return list
+		let shuffledArray = (self as? NSArray)?.shuffled()
+		let outputArray = shuffledArray as? [Iterator.Element]
+		return outputArray ?? []
 	}
-}
-
-extension MutableCollection where Index == Int {
-	
-	/// Shuffle the elements of `self` in-place.
-	mutating func shuffleInPlace() {
-
-		if count < 2 { return }
-		
-		for i in 0..<count.hashValue {
-			let j = Int(arc4random_uniform(UInt32(count.hashValue - i))) + i
-			guard i != j else { continue }
-			self.swapAt(i, j)
+	mutating func shuffle() {
+		if let selfShuffled = self.shuffled() as? Self {
+			self = selfShuffled
 		}
 	}
 }
-
