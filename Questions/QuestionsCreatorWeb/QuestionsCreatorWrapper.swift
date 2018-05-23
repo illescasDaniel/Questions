157521acea1bdb2674066df5919dd3028ad47af6
illescasDaniel <illescas.daniel@protonmail.com>
2018-05-23 23:36:43 +0200

    removed lag when tapping on buttons in Topic Creator

--- Questions/QuestionsCreatorWeb/QuestionsCreatorWrapper.swift
+++ Questions/QuestionsCreatorWeb/QuestionsCreatorWrapper.swift
@@ -27,6 +27,7 @@ struct QuestionsCreatorWrapper {
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
 """
 		
 		let extraStyles = """
@@ -54,13 +55,13 @@ struct QuestionsCreatorWrapper {
 		</style>
 """
 		let scripts = """
-		<script>			
+		<script>
 			function hideSectionWithButton(sectionID, buttonID) {
 				
 				const optionsSection = document.getElementById(sectionID)
 				const optionsButton = document.getElementById(buttonID)
 				
-				optionsButton.onclick = function () {
+				optionsButton.ontouchstart = function () {
 					if (optionsSection.style.display == "none" || optionsSection.style.display == "") {
 						optionsSection.style.display = "block"
 						optionsButton.textContent = "-"
@@ -190,6 +191,7 @@ struct QuestionsCreatorWrapper {
 		<html>
 			<head>
 				<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, user-scalable=no">
 				<title>Creator Web</title>
 		        \(bootstrapp)
 		        \(scripts)
